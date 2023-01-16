b78297e3a2b0b9cfbe2802b36511292ff8ef1f5c
Paul Morris <creative2012@hotmail.com>
2023-01-16 18:01:53 +0000

    added hamburger

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,13 @@
 const pageContainer = document.querySelector('main');
+const bigMac = document.querySelector('#hamburgerWrapper');
 var timeoutID = null;
+var hamburger = false;
+//is Iphone?
+function isIphone() {
+    if (navigator.userAgent.match(/iPhone/i)) {
+
+    }
+}
 
 //function to show homepage
 function popHomepage(headerText) {
@@ -8,11 +16,11 @@ function popHomepage(headerText) {
     container.innerHTML = '';
     // container.style.setProperty('height', '30px');
     // container.innerHTML = '<div class="homeDes">A collection of CSS styles you can easily copy and be inspired from, for your own projects</div>';
-    container.setAttribute('class','hide');
-    header.innerHTML = 
-    '<div class="helloMsg">#cssArtisan {<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span style="color:#efff00;">Welcome:</span>&nbsp "Hello"&nbsp ;<br/>}</div>';
+    container.setAttribute('class', 'hide');
+    header.innerHTML =
+        '<div class="helloMsg">#cssArtisan {<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span style="color:#efff00;">Welcome:</span>&nbsp "Hello"&nbsp ;<br/>}</div>';
     header.setAttribute('class', 'home');
-    
+
 
 }
 //Function to show other pages
@@ -20,8 +28,8 @@ function popPage(pageContent) {
     var header = document.querySelector('#section-header');
     var container = document.querySelector('#main-container');
     container.innerHTML = '';
-    header.removeAttribute('class','home');
-    container.removeAttribute('class','hide');
+    header.removeAttribute('class', 'home');
+    container.removeAttribute('class', 'hide');
     header.textContent = 'Box Shadows';
     var i = 1;
     pageContent.forEach(function (style) {
@@ -77,42 +85,56 @@ function boxAlert(button) {
 
 //function to control active nav itom
 function navController(button, change = true) {
-    if(change) button.style.color = 'black' ;
+    if (change) button.style.color = 'black';
     var inactive = document.querySelector('.active');
     inactive.classList.remove('active');
-    if(inactive.id != 'nav-title') inactive.style.color = 'lightslategrey';
+    if (inactive.id != 'nav-title') inactive.style.color = 'lightslategrey';
     button.classList.add('active');
 
+}
+//close hamburger menu
+function isHamburger(){
+
+        hamburger = false;
+        bigMac.removeAttribute('style','max-height');
+    
 }
 
 //function to handle button clicks
 function buttonHandler(button) {
+    
     if (button.id == 'nav-title') {
+     
         navController(button, false);
         popHomepage('home');
 
     }
     if (button.id == 'boxShadowLink') {
+     
         navController(button);
         popPage(shadowBoxes);
 
     }
     if (button.id == 'checkBoxesLink') {
+     
         navController(button);
         popGeneric('Check Boxes')
 
     }
     if (button.id == 'buttonsLink') {
+       
         navController(button);
         popGeneric('Buttons')
 
     }
     if (button.id == 'scrollLink') {
+        
         navController(button);
         popGeneric('Scroll bars')
 
     }
     if (button.id == 'backgroundLink') {
+        
         navController(button);
         popGeneric('Backgrounds')
 
@@ -123,7 +145,17 @@ function buttonHandler(button) {
     if (button.parentElement.classList.contains('shadow-box')) {
         copyToClip(button.parentElement);
     }
+    if (button.id == 'ham' && !hamburger) {
+        hamburger = true;
+        bigMac.setAttribute('style','max-height: 222px');
+        return;
+    } 
+    if(hamburger){
+       
+        isHamburger();
+    } 
 }
+// isIphone();
 popHomepage('home');
 //page click listener
 const navContainer = document.querySelector('body');
